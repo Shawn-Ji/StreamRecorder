c1200c0921b4c18709c014c6a13b170aacaebd89
FortuneDayssss <717622995@qq.com>
2019-12-19 08:46:33 +0800

    auto deploy test

--- deploy.py
+++ deploy.py
@@ -15,10 +15,10 @@ def deploy():
     with settings(warn_only=True):
         with cd(workspace_dir):
             if run("test -d {}".format(code_dir)).failed:
-                run("git clone https://github.com/FortuneDayssss/StreamRecorder.git {}".format(code_dir))
+                run("git clone --branch dev https://github.com/FortuneDayssss/StreamRecorder.git {}".format(code_dir))
                 run("mkdir -p video log")
     with cd(code_dir):
-        run("git pull")
+        run("git pull origin dev")
     with cd(code_dir + "/docker"):
         run("docker-compose up")
 
